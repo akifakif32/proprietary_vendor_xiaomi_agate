ef02d87ac79b39170c69c005445ebe2fcb4e2271
exerczz1 <exrcz513@gmail.com>
2026-07-25 09:15:53 +0000

    Initial commit: add agate vendor blobs and configs

---
+++ Android.mk
@@ -0,0 +1,27 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),agate)
+
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,8daafe6b05ed03c39ae3455ebc4e552982eeb4a5)
+$(call add-radio-file-sha1-checked,radio/cam_vpu1.img,a745614c02fde9a3d2d7c81074297d1138b5c7d4)
+$(call add-radio-file-sha1-checked,radio/cam_vpu2.img,7933310f65f6239a6f334ee95cc25e5dff0e0862)
+$(call add-radio-file-sha1-checked,radio/cam_vpu3.img,60441c63122cf6925e2d0389827913ef06c1515c)
+$(call add-radio-file-sha1-checked,radio/dpm.img,2e593481daf1577a7284298a377ba69c69dcca2f)
+$(call add-radio-file-sha1-checked,radio/dtbo.img,8fa40af58e98bd05544dabe7ba9ab25b955f2981)
+$(call add-radio-file-sha1-checked,radio/gz.img,4303c824494348be34bf53f733cb91bede610bdb)
+$(call add-radio-file-sha1-checked,radio/lk.img,605ede19de841a692611823c0972893bc3d4dcdd)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,5f3173b1d41e5178a822753cb95cc88cf3463e40)
+$(call add-radio-file-sha1-checked,radio/md1img.img,1ccb5702a746b133df4b036db7c1cc9c1ac54598)
+$(call add-radio-file-sha1-checked,radio/mitee.img,58c159ab3b2f4274706124b3fb1f2f437f6b6ef5)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,83bb537162398e8d7ba308fe536a880e5be20d6a)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,35ea4e94d2dfe957ac5b5be1c4f3c94c3cff500b)
+$(call add-radio-file-sha1-checked,radio/scp.img,2723a96b222c326ed54f10f1d2af2f58525d5de0)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,23344fae982b9f597aba62092fdc56f6c266ade8)
+$(call add-radio-file-sha1-checked,radio/sspm.img,4e471126d17ade8396803966d8d1f3bc33fc05a3)
+$(call add-radio-file-sha1-checked,radio/tee.img,0ded8eaeec53a736474be3501dd9732c5c9e16f5)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,22 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    audio_dsp \
+    cam_vpu1 \
+    cam_vpu2 \
+    cam_vpu3 \
+    dpm \
+    dtbo \
+    gz \
+    lk \
+    mcupm \
+    md1img \
+    mitee \
+    pi_img \
+    preloader_raw \
+    scp \
+    spmfw \
+    sspm \
+    tee
